f36e74d50e6dc56b315b8dddd210d194bb6df9fe
Molly <mollybick94@gmail.com>
2019-10-26 16:42:12 -0500

    Added firebase. Ability to create new users, add new recipes.

--- src/js/login.js
+++ src/js/login.js
@@ -1,37 +1,94 @@
 // eslint-disable-next-line no-unused-vars
-function submitForm() {
-    if(validateUser(document.getElementById('existingUser').value, document.getElementById('existingPassword').value)) {
-        window.open('src/html/Welcome.html', '_self')
+// eslint-disable-next-line no-undef
+const signInButton = document.querySelector("#signIn");
+const signUpButton = document.querySelector("#signUp");
+
+
+function addCategories(username, category) {
+    // eslint-disable-next-line no-undef
+    return db.collection('users').doc(username).collection('recipes')
+        .doc(category).set({category}).then(() => true);
+}
+
+
+if(signInButton)
+signInButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const validUsers = await getUsers();
+    return submitForm(validUsers)
+});
+
+
+if(signUpButton)
+signUpButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const validUsers = await getUsers();
+    let username = document.getElementById('newUser').value;
+    let password = document.getElementById('newPassword').value;
+    let reenterPassword = document.getElementById('reenterPassword').value;
+    if(validateNewUser(validUsers, username, password, reenterPassword)) {
+        await createUser(username, password);
+        alert("New user successfully created");
+        document.getElementById('newUser').value = '';
+        document.getElementById('newPassword').value = '';
+        document.getElementById('reenterPassword').value = '';
+    }
+});
+
+function createUser(userName, password) {
+    // eslint-disable-next-line no-undef
+        return db.collection('users').doc(userName).set({
+            username:userName,
+            password
+        }).then(async () => {
+            await addCategories(userName, 'breakfast');
+            await addCategories(userName, 'lunch');
+            await addCategories(userName, 'dinner');
+            return true;
+    });
+}
+
+function submitForm(validUsers) {
+    let username = document.getElementById('existingUser').value;
+    let password = document.getElementById('existingPassword').value;
+    if(validateUser(validUsers, username, password)) {
+        let openPage = 'src/html/Welcome.html?username=' + username;
+        window.open(openPage, '_self')
     } else {
         alert("Incorrect username or password")
     }
 }
-const users = [
-    {
-        username: "Terrie",
-        password: "TBick"
-    },
-    {
-        username: "Greg",
-        password: "GBick"
-    },
-    {
-        username: "Molly",
-        password: "MolBick"
-    },
-    {
-        username: "Madison",
-        password: "MadBick"
-    },
-    {
-        username: "Mackenzie",
-        password: "MackBick"
-    },
-];
 
- function validateUser(userName, password) {
-    let isValidUser = users.filter(user => {
+function getUsers() {
+    // eslint-disable-next-line no-undef
+    return db.collection('users').get().then((snapshot) => {
+        let users = [];
+        snapshot.docs.forEach((doc) => {
+            users.push(doc.data());
+        });
+        return users;
+    });
+}
+
+function validateNewUser(validUsers, username, password, reenterPassword) {
+     if(isNewUserName(validUsers, username)) {
+         if(password === reenterPassword) {
+             return true;
+         }
+         alert("Password don't match");
+         return false;
+     }
+     alert("Username already exists, try a new one");
+     return false;
+}
+
+function isNewUserName(validUsers, newUsername){
+    return validUsers.filter(user => user.username === newUsername).length === 0;
+}
+
+function validateUser(users, userName, password) {
+    let isValid = users.filter(user => {
         return user.username === userName && user.password === password
     });
-    return isValidUser.length > 0;
+    return isValid.length>0
 }
